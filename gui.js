df6cc306dcde187b2599f3b21eb90e47442937cb
MIkael Monteiro <mikaelmonteiro@gmail.com>
2023-02-08 15:12:40 +0000

    fix: change gui.js with forEach method

--- gui.js
+++ gui.js
@@ -1,13 +1,24 @@
+// export function imprimeExtrato(extractList) {
+//     let lista = "";
+//     for (let i = 0; i < extractList.length; i++) {
+//         lista += `
+//         tipo: ${extractList[i].nome}<br>
+//         valor: ${extractList[i].valor}<br>
+//         data: ${extractList[i].data}<br>
+//         `;
+//     }
+//     return lista;
+// }
+
 export function imprimeExtrato(extractList) {
     let lista = "";
-    for (let i = 0; i < extractList.length; i++) {
-        lista += `
-        tipo: ${extractList[i].nome}<br>
-        valor: ${extractList[i].valor}<br>
-        data: ${extractList[i].data}<br>
-        `;
-    }
+    extractList.forEach((item) => {
+      lista += `
+      tipo: ${item.nome}<br>
+      valor: ${item.valor}<br>
+      data: ${item.data}<br>
+      `;
+    });
     return lista;
-}
-
-
+  }
+  
