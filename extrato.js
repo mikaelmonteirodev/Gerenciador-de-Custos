38708011d4aad231233797951a6fd9d3c068c43e
MIkael Monteiro <112489130+mikaelmonteirodev@users.noreply.github.com>
2023-02-09 16:18:31 -0300

    fix: coments

--- extrato.js
+++ extrato.js
@@ -1,15 +1,15 @@
-const expenses = [
-  { value: 50.0, date: "2022-12-01", type: "food" },
-  { value: 20.0, date: "2022-12-02", type: "transportation" },
-  { value: 10.0, date: "2022-12-03", type: "entertainment" }
-];
+// const expenses = [
+//   { value: 50.0, date: "2022-12-01", type: "food" },
+//   { value: 20.0, date: "2022-12-02", type: "transportation" },
+//   { value: 10.0, date: "2022-12-03", type: "entertainment" }
+// ];
 
-let totalExpenses = 0;
+// let totalExpenses = 0;
 
-console.log("Extrato de gastos:");
-expenses.forEach(expense => {
-  console.log(`Valor: ${expense.value} | Data: ${expense.date} | Tipo: ${expense.type}`);
-  totalExpenses += expense.value;
-});
+// console.log("Extrato de gastos:");
+// expenses.forEach(expense => {
+//   console.log(`Valor: ${expense.value} | Data: ${expense.date} | Tipo: ${expense.type}`);
+//   totalExpenses += expense.value;
+// });
 
-console.log(`Total de gastos: ${totalExpenses}`);
+// console.log(`Total de gastos: ${totalExpenses}`);
